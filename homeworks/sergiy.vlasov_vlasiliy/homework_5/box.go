015f6031a244025a65ac4f724daf391d9ed6cba3
Sergey Vlasov <vlasiliy@gmail.com>
2017-12-20 16:17:38 +0200

    fix

--- homeworks/sergiy.vlasov_vlasiliy/homework_5/box.go
+++ homeworks/sergiy.vlasov_vlasiliy/homework_5/box.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/vlasiliy/go-course-2017/homeworks/sergiy.vlasov_vlasiliy/homework_5/shapes"
+	"github.com/MastersAcademy/go-course-2017/homeworks/sergiy.vlasov_vlasiliy/homework_5/shapes"
 	"math/rand"
 	"fmt"
 )
